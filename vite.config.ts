bf58b16a881fc95830dfcd6a21fd76e5eef9e9e6
Johann Pardanaud <johann@pardanaud.com>
2026-01-05 21:43:00 +0100

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/tumblin-dice/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
